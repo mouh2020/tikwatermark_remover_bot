3ed11e22c49e014641f8cd1402f951409125e0ee
mouh2020 <badimohammed2019@gmail.com>
2023-07-21 16:07:27 +0100

    fix some errors related to message_id

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ bot = telebot.TeleBot(bot_token)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\n
-Hi there, I'am Watermark Remover Bot.
-I'am here to remove watermark from TikTok videos. Just send the link and i will send you the video to downlaod without watermark !\n
+Hi there, I'am TikTok Watermark Remover Bot.
+I'am here to remove watermarks from TikTok videos.\nJust send the link and i will send you the video to downlaod without watermark !\n
 """)
 
 
@@ -23,29 +23,38 @@ I'am here to remove watermark from TikTok videos. Just send the link and i will 
 def echo_message(message):
     link = message.text
     message_id = bot.reply_to(message,"Wait until checking URL's validity.").id
+    logger.info(f'message from : {str(message.from_user.id)} link : {str(message.text)}')
     if "https://" in message.text and "tiktok.com" in message.text : 
-        bot.edit_message_text('Converting the link ...')
-        tiktok_video_link : str = get_valid_link(tiktok_link=message.text)
+        bot.edit_message_text(chat_id=message.from_user.id,
+                              text='Converting the link ...',
+                              message_id=message_id)
+        
+        tiktok_video_link  = get_valid_link(tiktok_link=message.text)
+        logger.info(f'convert link to : {str(tiktok_video_link)}')
         if tiktok_video_link :
-            bot.edit_message_text(f"Removing watermark ...",
-                                chat_id=message.from_user.id,
-                                message_id=message_id) 
+            bot.edit_message_text(text="Removing watermark ...",
+                                  chat_id=message.from_user.id,
+                                  message_id=message_id) 
             video_link = download_video(tiktok_video_link)
             if video_link :
-                bot.edit_message_text(f"Watermark was removed successfully 👌",
-                                        chat_id=message.from_user.id,
-                                        message_id=message_id)
+                bot.edit_message_text(text=f"Watermark was removed successfully 👌",
+                                      chat_id=message.from_user.id,
+                                      message_id=message_id)
                 bot.send_video(message.from_user.id,
                                video_link)
                 return
             
-            bot.edit_message_text(f"Unable to remove watermark ❌",
-                             chat_id=message.from_user.id,
-                             message_id=message_id) 
+            bot.edit_message_text(text="Unable to remove watermark ❌",
+                                  chat_id=message.from_user.id,
+                                  message_id=message_id) 
             return
-        bot.edit_message_text("Unable to convert the link.")
+        bot.edit_message_text(chat_id= message.from_user.id,
+                              text="Unable to convert the link.",
+                              message_id=message_id)
         return
-    bot.edit_message_text("Please enter a valid TikTok link.")
+    bot.edit_message_text(chat_id= message.from_user.id,
+                          text="Please enter a valid TikTok link.",
+                          message_id=message_id)
             
         
                    
